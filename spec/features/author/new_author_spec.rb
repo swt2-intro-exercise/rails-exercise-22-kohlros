fd7f9cb65b9e5a19e75262264e4a4f4a89d74996
kohlros <kohlros@uni-potsdam.de>
2022-10-28 09:55:47 +0200

    fix #3 spelling fix

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -7,8 +7,8 @@ describe "New author page", type: :feature do
   end
   it "should have text input for an authors firstname, lastname and homepage" do
     visit new_author_path
-    expect(page).to have_field('author[firstname]')
-    expect(page).to have_field('author[lastname]')
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
 end
