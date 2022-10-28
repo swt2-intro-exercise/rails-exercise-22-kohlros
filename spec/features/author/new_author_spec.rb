dabddeebd02ba7a838539c57843424d8008ce4e3
kohlros <kohlros@uni-potsdam.de>
2022-10-28 09:33:04 +0200

    add failing test for #2

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,10 @@ describe "New author page", type: :feature do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
   end
+  it "should have text input for an authors firstname, lastname and homepage" do
+    visit new_author_path
+    expect(page).to have_field('author[firstname]')
+    expect(page).to have_field('author[lastname]')
+    expect(page).to have_field('author[homepage]')
+  end
 end
