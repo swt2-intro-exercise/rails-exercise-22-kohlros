1c10ebcba15bb5fdf43a087c632222f7c8ef2ab2
kohlros <kohlros@uni-potsdam.de>
2022-10-28 10:28:46 +0200

    add author model to fix #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Author model", type: :model do
     it "should have firstname, lastname,homepage and return the name correctly" do
-        author = Author.new(first_name="Alan",last_name="Turing",homepage="http://wikipedia.org/Alan_Turing")
+        author = Author.new(:first_name => "Alan", :last_name => "Turing", :homepage => "http://wikipedia.org/Alan_Turing")
         expect(author.first_name).to eq("Alan")
         expect(author.last_name).to eq("Turing")
         expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
