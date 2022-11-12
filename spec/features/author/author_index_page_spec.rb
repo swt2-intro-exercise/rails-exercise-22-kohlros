b3c8d38ff59d9d0ca1d74c56a17538a458b264ed
kohlros <kohlros@uni-potsdam.de>
2022-11-12 14:04:37 +0100

    add failing tests for #7

---
+++ spec/features/author/author_index_page_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Show overview of all authors", type: :feature do
+    before :each do
+        @alan = FactoryBot.create :author
+        @alan.save
+
+        visit author_index
+    end
+    it "should exist" do
+    end
+    it "should display all Author names and homepages " do
+        expect(page).to have_text("Name")
+        expect(page).to have_text("Homepage")
+        expect(page).to have_text("Alan Turing")
+        expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+    end
+    it "should display links to all author pages and the create page" do
+        expect(page).to have_link , href:author_path(@alan)
+        expect(page).to have_link 'New', href: new_author_path
+    end
+end
